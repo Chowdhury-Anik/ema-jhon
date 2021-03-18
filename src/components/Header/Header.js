c85f9ca6428261e0ca25017a186b37e80f409d1e
Chowdhury-Anik <bloggeranik1986@gmail.com>
2021-03-19 00:19:01 +0600

    react form impelemented

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../App';
 import logo from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
     return (
         <div className='header'>
@@ -16,6 +18,7 @@ const Header = () => {
                 <Link to="review">Order Review</Link>
                 <Link to="shipment">Shipment</Link>
                 <Link to="login">Login</Link>
+                <button onClick={() => setLoggedInUser({})}>Sign out</button>
             </nav>
         </div>
     );
